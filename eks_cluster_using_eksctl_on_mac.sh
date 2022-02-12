a45337740de573716638f56bd8cb329e44e3e2cb
Sharper than Learning Curve <productiveAnalytics@users.noreply.github.com>
2022-02-12 17:12:54 -0600

    EKS cluster creation using EKSctl on MacOS

---
+++ eks_cluster_using_eksctl_on_mac.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Update homebrew to get EKSCtl
+brew tap weaveworks/tap
+brew install weaveworks/tap/eksctl
+
+# Confirm eksctl
+eksctl version
+
+# EKSctl needs access to AWS session
+# login using "aws-saml-auth" to establish AWS session
+aws sts get-caller-identity
+
+# Create cluster with supported Kubernetes version
+export EKS_SUPPORTED_K8S_VERSION=1.21
+eksctl create cluster \
+--name laap-eks-ue1-test-cluster-sbx \
+--version ${EKS_SUPPORTED_K8S_VERSION} \
+--region us-east-1 \
+--nodegroup laap-k8s-nodes \
+--node-type t2.micro \
+--nodes 3
