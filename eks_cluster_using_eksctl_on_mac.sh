41a6721da63e2a17bf73f6ab08425ebbe3fe0a4d
Sharper than Learning Curve <productiveAnalytics@users.noreply.github.com>
2022-02-12 17:53:02 -0600

    Added kubectl comands for confirmation EKS cluster

--- eks_cluster_using_eksctl_on_mac.sh
+++ eks_cluster_using_eksctl_on_mac.sh
@@ -13,8 +13,10 @@ aws sts get-caller-identity
 
 # Create cluster with supported Kubernetes version
 export EKS_SUPPORTED_K8S_VERSION=1.21
+export EKS_CLUSTER_NAME=laap-eks-ue1-test-cluster-sbx
+
 eksctl create cluster \
---name laap-eks-ue1-test-cluster-sbx \
+--name ${EKS_CLUSTER_NAME} \
 --version ${EKS_SUPPORTED_K8S_VERSION} \
 --region us-east-1 \
 --nodegroup-name laap-k8s-nodes \
@@ -23,12 +25,21 @@ eksctl create cluster \
 
 # To check the stack
 eksctl utils describe-stacks \
---region=us-east-1 \
---cluster=laap-eks-ue1-test-cluster-sbx
+--region us-east-1 \
+--cluster ${EKS_CLUSTER_NAME}
 
 # To enable CloudWatch events for EKS cluster
 eksctl utils update-cluster-logging \
 --enable-types=all \
---region=us-east-1 \
---cluster=laap-eks-ue1-test-cluster-sbx \
+--region us-east-1 \
+--cluster ${EKS_CLUSTER_NAME} \
 --approve
+
+# Typically EKSctl install KubeConfig under ~/.kube/config
+# Run KubeCtl commands, like
+kubectl get nodes
+kubectl get ns
+kubectl get pod
+
+# TO CLEAN-UP
+# eksctl delete cluster --name ${EKS_CLUSTER_NAME}
