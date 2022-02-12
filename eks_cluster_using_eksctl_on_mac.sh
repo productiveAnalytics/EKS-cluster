2581111c971835af8904a7a0f8e730b451517fb9
Sharper than Learning Curve <productiveAnalytics@users.noreply.github.com>
2022-02-12 17:18:47 -0600

    Debug "eksctl utils describe-stacks" for stack

--- eks_cluster_using_eksctl_on_mac.sh
+++ eks_cluster_using_eksctl_on_mac.sh
@@ -20,3 +20,8 @@ eksctl create cluster \
 --nodegroup-name laap-k8s-nodes \
 --node-type t2.micro \
 --nodes 3
+
+# To check the stack
+eksctl utils describe-stacks \
+--region=us-east-1 \
+--cluster=laap-eks-ue1-test-cluster-sbx
