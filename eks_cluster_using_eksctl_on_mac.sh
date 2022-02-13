0c2354e979e3e2f6ffda7486fbf01dd7d4bb107f
Sharper than Learning Curve <productiveAnalytics@users.noreply.github.com>
2022-02-12 18:24:12 -0600

    Added simple and complex version of EKS cluster
    eksctl create cluster --help

--- eks_cluster_using_eksctl_on_mac.sh
+++ eks_cluster_using_eksctl_on_mac.sh
@@ -14,7 +14,10 @@ aws sts get-caller-identity
 # Create cluster with supported Kubernetes version
 export EKS_SUPPORTED_K8S_VERSION=1.21
 export EKS_CLUSTER_NAME=laap-eks-ue1-test-cluster-sbx
+export EKS_MIN_NODES=3
+export EKS_MAX_NODES=5
 
+# Simple cluster with static ASG and in new VPC and nodes as EC2
 eksctl create cluster \
 --name ${EKS_CLUSTER_NAME} \
 --version ${EKS_SUPPORTED_K8S_VERSION} \
@@ -22,6 +25,23 @@ eksctl create cluster \
 --nodegroup-name laap-k8s-nodes \
 --node-type t2.micro \
 --nodes 3
+
+# Complex cluster with Auto Scaler, and in existing private subnets of VPC and Fargate based nodes
+eksctl create cluster \
+--name ${EKS_CLUSTER_NAME} \
+--tags author=ProductiveAnalytics usage=test \
+--version ${EKS_SUPPORTED_K8S_VERSION} \
+--region us-east-1 \
+--nodegroup-name laap-k8s-nodes \
+--node-type t2.micro \
+--nodes-min ${EKS_MIN_NODES} \
+--nodes-max ${EKS_MAX_NODES} \
+--node-ami-family Ubuntu2004 \
+--instance-prefix my_eks \
+--vpc-private-subnets  <PUT_VPC_Private_Subnect_IDs> \
+--enable-ssm \
+--asg-access \
+--fargate
 
 # To check the stack
 eksctl utils describe-stacks \
