05bae8d0aeba0b8e34827533af4ae862d2ed7094
Sharper than Learning Curve <productiveAnalytics@users.noreply.github.com>
2022-02-13 23:40:34 -0600

    Update eks_cluster_using_eksctl_on_mac.sh
    1. kubectl get services --all-namespaces
    2. Look service entry for TYPE=LoadBalancer
    3. Note the EXTERNAL-IP that looks like AWS ELB (e.g. ae5fbf2cf3a134c68bc273a7cd58b51b-959518723.us-east-1.elb.amazonaws.com)
    4. ```curl -X ae5fbf2cf3a134c68bc273a7cd58b51b-959518723.us-east-1.elb.amazonaws.com``` or visit the page in browser

--- eks_cluster_using_eksctl_on_mac.sh
+++ eks_cluster_using_eksctl_on_mac.sh
@@ -21,6 +21,8 @@ export EKS_CLUSTER_NAME=laap-eks-ue1-test-cluster-${EKS_ENV_NAME}
 export EKS_MIN_NODES=3
 export EKS_MAX_NODES=5
 
+# NOTE: USE availability zones which match with VPC/Subnets
+
 # Simple EC2-based cluster that gets created in NEW VPC
 eksctl create cluster \
 --name ${EKS_CLUSTER_NAME} \
@@ -40,6 +42,7 @@ eksctl create cluster \
 --nodes 3 \
 --instance-prefix my_eks \
 --vpc-private-subnets  <PUT_VPC_Private_Subnect_IDs> \
+--zones us-east-1a,us-east-1b,us-east-1c \
 --enable-ssm \
 --asg-access \
 --alb-ingress-access
@@ -56,6 +59,7 @@ eksctl create cluster \
 --nodes-max ${EKS_MAX_NODES} \
 --instance-prefix my_eks \
 --vpc-private-subnets  <PUT_VPC_Private_Subnect_IDs> \
+--zones us-east-1a,us-east-1b,us-east-1c \
 --enable-ssm \
 --asg-access \
 --alb-ingress-access \
@@ -79,7 +83,9 @@ kubectl get nodes
 kubectl get ns
 kubectl get deployments
 kubectl get services
-kubectl get pod --all-namespaces -o wide
+kubectl get pods --all-namespaces -o wide
+
+## To confirm working of Kubernetes pods: either use "cUrl -X GET" or browser to visit the EXTERNAL-IP from the output of "kubectl get services" for TYPE=LoadBalancer
 
 # TO CLEAN-UP
 # eksctl delete cluster --name ${EKS_CLUSTER_NAME}
