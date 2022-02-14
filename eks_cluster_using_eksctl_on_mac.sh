d3aef675c850f7b22a82930fbfda063515cedff4
Sharper than Learning Curve <productiveAnalytics@users.noreply.github.com>
2022-02-13 21:40:00 -0600

    Updated commands for EC2 and Fargate node styles

--- eks_cluster_using_eksctl_on_mac.sh
+++ eks_cluster_using_eksctl_on_mac.sh
@@ -23,23 +23,24 @@ export EKS_MAX_NODES=5
 # Simple cluster with static ASG and in new VPC and nodes as EC2
 eksctl create cluster \
 --name ${EKS_CLUSTER_NAME} \
+--tags author=ProductiveAnalytics,usage=test,node_style=ec2 \
 --version ${EKS_SUPPORTED_K8S_VERSION} \
 --region us-east-1 \
---nodegroup-name laap-k8s-nodes \
+--nodegroup-name laap-k8s-ec2-nodes \
 --node-type t2.micro \
+--node-ami-family Ubuntu2004 \
 --nodes 3
 
 # Complex cluster with Auto Scaler, and in existing private subnets of VPC and Fargate based nodes
 eksctl create cluster \
 --name ${EKS_CLUSTER_NAME} \
---tags author=ProductiveAnalytics,usage=test \
+--tags author=ProductiveAnalytics,usage=test,node_style=fargate \
 --version ${EKS_SUPPORTED_K8S_VERSION} \
 --region us-east-1 \
---nodegroup-name laap-k8s-nodes \
+--nodegroup-name laap-k8s-fargate-nodes \
 --node-type t2.micro \
 --nodes-min ${EKS_MIN_NODES} \
 --nodes-max ${EKS_MAX_NODES} \
---node-ami-family Ubuntu2004 \
 --instance-prefix my_eks \
 --vpc-private-subnets  <PUT_VPC_Private_Subnect_IDs> \
 --enable-ssm \
